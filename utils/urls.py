10ffaf579f7d94cb8c440a090ef6b9124aef36f8
Szymon Karpiński <hugo@staszic.waw.pl>
2019-01-29 20:03:58 +0000

    reloading time limits from config

--- utils/urls.py
+++ utils/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
 import views
 
-contest_patterns = [url(r'^example-tests/(?P<pi_short>[a-z0-9-]+)/$', views.example_tests_view, name='example-tests')]
-
+contest_patterns = [url(r'^example-tests/(?P<pi_short>[a-z0-9-]+)/$', views.example_tests_view, name='example-tests'),
+    url(r'p/(?P<problem_instance_id>[a-z0-9_-]+)/reload_tests_limits_for_probleminstance', views.reload_limits_from_config_view, name='reload_tests_limits_for_probleminstance')]
--- utils/views.py
+++ utils/views.py
@@ -18,6 +18,8 @@ from models import *
 from oioioi.filetracker.utils import stream_file
 import zipfile
 from StringIO import StringIO
+from oioioi.sinolpack.package import _stringify_keys
+import re
 
 @enforce_condition(contest_exists & can_enter_contest)
 def example_tests_view(request, pi_short):
@@ -52,3 +54,26 @@ def example_tests_view(request, pi_short):
 
     return response
 
+
+@enforce_condition(contest_exists & can_enter_contest & is_superuser)
+def reload_limits_from_config_view(request, problem_instance_id):
+    pi = get_object_or_404(ProblemInstance, id=problem_instance_id)
+    config = pi.problem.extraconfig.parsed_config
+    tests = [t for t in pi.test_set.filter()]
+    log = []
+    for t in tests:
+        name = t.name
+        tl_dict = _stringify_keys(config['time_limits'])
+        original_tl = t.time_limit
+        new_tl = tl_dict.get(name, tl_dict.get(re.search('^\d+', name).group(0), None))
+        if new_tl is None:
+            info = 'No time limit in config. Keeping old limit (%d)' % original_tl
+        elif new_tl == original_tl:
+            info = 'Time limits are equal (%d)' % original_tl
+        else:
+            t.time_limit = new_tl
+            t.save()
+            info = 'Time limit changed from %s to %s' % (str(original_tl), str(new_tl))
+        log.append({'test': name, 'info': info})
+    return render(request, 'utils/reload_limits.html', {'log': log,
+        'back': reverse('oioioiadmin:contests_probleminstance_changelist')})
