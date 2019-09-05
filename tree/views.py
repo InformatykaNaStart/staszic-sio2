e77bb7c32b9c804ec0770ad569077f8e1e3f9e5b
Szymon Karpinski <hugo@staszic.waw.pl>
2019-09-05 22:46:31 +0200

    filtering visible contests by regex

--- tree/views.py
+++ tree/views.py
@@ -8,10 +8,14 @@ from django.contrib.auth.hashers import check_password
 from django.http import HttpResponseForbidden, Http404, HttpResponse
 from oioioi.base.menu import menu_registry
 from oioioi.base.main_page import register_main_page_view
+from django.conf import settings
+import re
 
 @register_main_page_view(order=90)
 def contests_tree_view(request):
     contests = sorted(visible_contests(request), key=lambda x: x.creation_date)[::-1]
+    if settings.STASZIC_CONTEST_MODE and not request.user.is_superuser:
+        contests = [contest for contest in contests if re.match(settings.STASZIC_CONTEST_MODE, contest.pk)]
 
     groups = {}
     for contest in contests:
