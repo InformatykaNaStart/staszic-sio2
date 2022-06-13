5eb7b22fdecba59bf5c655eed7a45df4f2d89a8c
Szymon Karpinski <hugo@staszic.waw.pl>
2022-06-13 23:55:10 +0200

    Add pd

--- pd/models.py
+++ pd/models.py
@@ -1,10 +1,14 @@
 from __future__ import unicode_literals
 from django.contrib.auth.models import User
+from oioioi.contests.models import Contest
 from django.db import models
 
 class PersonalDataPass(models.Model):
     user = models.OneToOneField(User)
+    #wildcard = models.BooleanField(default=False)
+    #contest = models.ManyToManyField(Contest, null=True, blank=True)
     def __unicode__(self):
-        return self.user.first_name + ' ' + self.user.last_name + '(' + self.user.username + ')'
+        r = self.user.first_name + ' ' + self.user.last_name + ' (' + self.user.username + ')'
+        return r
     class Meta:
         verbose_name_plural = "Personal Data Passes"
--- pd/permissions.py
+++ pd/permissions.py
@@ -1,6 +1,12 @@
 import models
 from oioioi.base.permissions import make_request_condition
+from oioioi.contests.models import ContestPermission
 
 @make_request_condition
 def has_personal_data_pass(request):
     return models.PersonalDataPass.objects.filter(user=request.user).exists()
+
+@make_request_condition
+def has_contest_personal_data_access(request):
+    return models.PersonalDataPass.objects.filter(user=request.user).exists() or \
+        ContestPermission.objects.filter(user=request.user, contest=request.contest).exists()
